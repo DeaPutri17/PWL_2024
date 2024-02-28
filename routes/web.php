2ad996f087023d6225a64b75ac0d53dc71cdd086
DeaPutri17 <putrinastitidea@gmail.com>
2024-02-28 15:08:32 +0700

    modifikasi route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,42 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/hello', function (){
+    return 'Hello World';
+});
+
+Route::get('/world', function (){
+    return 'World';
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//tugas
+Route::get('/', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return '2241720117 <br>Dea Putri Nastiti';
+});
+
+Route::get('/user/{name} ', function($name){
+    return 'Nama saya ' .$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-' .$postId.' Komentar ke-: '.$commentId;
+});
+
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John'){
+    return 'Nama saya '.$name;
+});
+
+
